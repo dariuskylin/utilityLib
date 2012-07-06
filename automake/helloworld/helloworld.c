a35edc8e00351c3ea9c28150d37ae1aa2ee1d05d
dariuskylin <dongyuchi@gmail.com>
2012-07-05 20:31:58 -0700

    automake

---
+++ automake/helloworld/helloworld.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  helloworld.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  07/05/2012 08:00:49 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  dongyuchi (dongyuchi), dongyuchi@gmail.com
+ *        Company:  UESTC
+ *
+ * =====================================================================================
+ */
+#include<stdio.h>
+int main()
+{
+    printf("Hello,Linux world!\n");
+    return 0;
+}
