618d5a50b9b3560acfe555f816b92aa601d57ed7
dariuskylin <dongyuchi@gmail.com>
2012-09-28 17:45:02 +0800

    network

---
+++ book-note/Effective-C++/initial-test.cc
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  initial-test.cc
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  09/24/2012 11:06:33 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  dongyuchi (dongyuchi), dongyuchi@gmail.com
+ *        Company:  UESTC
+ *
+ * =====================================================================================
+ */
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> intV;
+    int i;
+    intV.push_back(i);
+    cout<<intV[0]<<endl;
+    return 0;
+}
